f7361ecb5a72400d4c420ff50b47b2aa9315b0de
vaibhav2898 <goyalvaibhav700@gmail.com>
2020-01-18 01:44:13 +0530

    Updates

---
+++ precache-manifest.f4800ac5d1cdb642d6e6935fda09572d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a86d78b10c82d74bf6ec68b80ee8dd6",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c8ee159bb4366e14f41a",
+    "url": "/monster-rolodex/static/css/main.bef5a005.chunk.css"
+  },
+  {
+    "revision": "20eb7b3612173e07f0b7",
+    "url": "/monster-rolodex/static/js/2.4f0a8636.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.4f0a8636.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8ee159bb4366e14f41a",
+    "url": "/monster-rolodex/static/js/main.d0a0990e.chunk.js"
+  },
+  {
+    "revision": "4cf4f8a7fe2234154a3a",
+    "url": "/monster-rolodex/static/js/runtime-main.11452f00.js"
+  }
+]);
